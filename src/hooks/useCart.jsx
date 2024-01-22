0ea85459ed968e1480956e10599d5b40189501c9
ahmed samir <ahmedsamirweb42@gmail.com>
2024-01-22 21:42:58 +0200

    Update cart-item query implementation

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,11 +1,12 @@
 import { CART_API_URL } from "constants";
 import { request } from "server/axios-utils";
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 const fetchCartList = () => {
   return request({ url: CART_API_URL });
 };
-const fetchCartItem = (itemId) => {
+const fetchCartItem = ({ queryKey }) => {
+  const itemId = queryKey[1];
   return request({ url: `${CART_API_URL}/${itemId}` });
 };
 const mutateCartList = (item) => {
@@ -16,7 +17,10 @@ export function useCartList() {
   return useQuery("cart-list", fetchCartList, {});
 }
 export function useCartItem(itemId) {
-  return useQuery("cart-item", [fetchCartItem, itemId], {});
+  return useQuery(["cart-item", itemId], fetchCartItem, {
+    select: (data) => data.data,
+    staleTime: 10000,
+  });
 }
 export function useMutateCartList() {
   return useMutation("cart-list", mutateCartList, {
